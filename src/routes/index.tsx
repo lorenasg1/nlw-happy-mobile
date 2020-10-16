0ab20b347207b24d51164b243626e87669830087
lorenasg1 <lorenasg1@gmail.com>
2020-10-16 12:12:54 -0300

    feat: add routes to orphanage and orphanage creation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,46 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import OrphanagesMap from '../screens/OrphanagesMap';
 import Orphanage from '../screens/Orphanage';
+import SelectMapPosition from '../screens/CreateOrphanage/SelectMapPosition';
+import OrphanageData from '../screens/CreateOrphanage/OrphanageData';
+import Header from '../components/Header';
 
 const { Navigator, Screen } = createStackNavigator();
 
 const Routes = () => {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
+      <Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: '#f2f3f5' },
+        }}
+      >
         <Screen name="OrphanagesMap" component={OrphanagesMap} />
-        <Screen name="Orphanage" component={Orphanage} />
+        <Screen
+          name="Orphanage"
+          component={Orphanage}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Orfanato" showCancel={false} />,
+          }}
+        />
+        <Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione o local no mapa" />,
+          }}
+        />
+        <Screen
+          name="OrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe  os dados ddo orfanato" />,
+          }}
+        />
       </Navigator>
     </NavigationContainer>
   );
